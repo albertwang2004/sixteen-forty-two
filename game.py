d5dcf98a7ddda13421651ca23a5c0bb376467098
Albert Wang <albert.wang.2004@gmail.com>
2023-12-08 02:53:03 -0500

    some minor cleanup and rules

--- game.py
+++ game.py
@@ -10,7 +10,7 @@ pygame.init()
 
 # Set up the display
 screen_width = 800
-screen_height = 600
+screen_height = 432
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("16.42")
 
@@ -58,7 +58,7 @@ while True:
         # Display the number of cards left
         cards_left_text = pygame.font.SysFont("Courier New", 30).render(f"{cards_left} cards left", True, (255, 255, 255))
         # Wipe this area
-        pygame.draw.rect(screen, (0, 0, 0), (screen_width - cards_left_text.get_width() - 100, screen_height - cards_left_text.get_height() - 100, cards_left_text.get_width() + 50, cards_left_text.get_height()))
+        pygame.draw.rect(screen, (0, 0, 0), (screen_width - cards_left_text.get_width() - 80, screen_height - cards_left_text.get_height() - 100, cards_left_text.get_width() + 50, cards_left_text.get_height()))
         screen.blit(cards_left_text, (screen_width - cards_left_text.get_width() - 50, screen_height - cards_left_text.get_height() - 100))
         
         # Calculate the elapsed time
@@ -67,7 +67,7 @@ while True:
         # Format the time as minutes and seconds
         time_text = pygame.font.SysFont("Courier New", 30).render(f"t = {elapsed_time:.3f}", True, (255, 255, 255))
         # Display the time in the bottom right corner
-        pygame.draw.rect(screen, (0, 0, 0), (screen_width - cards_left_text.get_width() - 100, screen_height - time_text.get_height() - 50, cards_left_text.get_width() + 50, time_text.get_height()))
+        pygame.draw.rect(screen, (0, 0, 0), (screen_width - cards_left_text.get_width() - 80, screen_height - time_text.get_height() - 50, cards_left_text.get_width() + 50, time_text.get_height()))
         screen.blit(time_text, (screen_width - time_text.get_width() - 50, screen_height - time_text.get_height() - 50))
         pygame.display.flip()  # Update the display
 
@@ -100,20 +100,20 @@ while True:
             answer_image = card_images[answer]
             # Scale it down
             scaled_answer_image = pygame.transform.scale(answer_image, (answer_image.get_width() // 3, answer_image.get_height() // 3))
-            screen.blit(scaled_answer_image, (screen_width // 2 - scaled_answer_image.get_width() // 2, screen_height // 2 - scaled_answer_image.get_height() // 2))
+            screen.blit(scaled_answer_image, (screen_width // 2 - scaled_answer_image.get_width() // 2, screen_height // 2 - 25 - scaled_answer_image.get_height() // 2))
 
             # Display a little text above to show that it's the answer
             answer_text = pygame.font.SysFont("Courier New", 30).render("Answer", True, (255, 255, 255))
-            screen.blit(answer_text, (screen_width // 2 - answer_text.get_width() // 2, screen_height // 2 - scaled_answer_image.get_height() // 2 - answer_text.get_height() - 10))
+            screen.blit(answer_text, (screen_width // 2 - answer_text.get_width() // 2, screen_height // 2 - 25 - scaled_answer_image.get_height() // 2 - answer_text.get_height() - 10))
 
             # Display elapsed time
             elapsed_time_text = pygame.font.SysFont("Courier New", 30).render(f"t = {elapsed_time:.3f}", True, (255, 255, 255))
-            screen.blit(elapsed_time_text, (screen_width // 2 - elapsed_time_text.get_width() // 2, screen_height // 2 + scaled_answer_image.get_height() // 2 + 10))
+            screen.blit(elapsed_time_text, (screen_width // 2 - elapsed_time_text.get_width() // 2, screen_height // 2 - 25 + scaled_answer_image.get_height() // 2 + 10))
 
             # Message beneath this saying space to restart
             if pygame.time.get_ticks() / 1000 - elapsed_time - time_offset > 1:
                 restart_text = pygame.font.SysFont("Courier New", 30).render("Press space to restart", True, (255, 255, 255))
-                screen.blit(restart_text, (screen_width // 2 - restart_text.get_width() // 2, screen_height // 2 + scaled_answer_image.get_height() // 2 + 10 + elapsed_time_text.get_height() + 10))
+                screen.blit(restart_text, (screen_width // 2 - restart_text.get_width() // 2, screen_height // 2 - 25 + scaled_answer_image.get_height() // 2 + 10 + elapsed_time_text.get_height() + 10))
 
         pygame.display.flip()  # Update the display
 
--- readme.md
+++ readme.md
@@ -5,6 +5,14 @@ This is a trainer for guessing the missing card in a deck of cards. The game is 
 
 ```python game.py```
 
+## Rules
+
+16.42 is a game to be played with a group of any number of people, but preferably between 1 and 14. Your goal is to minimize the time it takes to guess the card correctly, accounting for the number of people present.
+
+At the beginning of the game, a standard deck of cards is shuffled, and the bottom card is removed. Your job is to view all of the other cards one at a time, and guess what this bottom card is.
+
+You may only use whatever you can do with your body, including but not limited to counting on your fingers, memorizing values, and communicating with your team. That means no writing down digits, using calculators, etc.
+
 ## Installation
 
 You will need the `pygame` module, which can be simply installed by running
@@ -13,13 +21,13 @@ You will need the `pygame` module, which can be simply installed by running
 
 ## Changelog
 
+### v0.1
+
+Added simple visual interface
+
 ### Initial v0.0
 All but one of the 52 cards will be dealt in sequence, printed to console output.
 
 Before running the game, make sure you have the `keyboard` module installed. You can install it using the following command:
 
 ```pip install keyboard```
-
-### v0.1
-
-Added simple visual interface
